324637261e4117d74f02d7a993ebc11017c95fa2
Robert Medeiros <robert@crimeminister.org>
2013-09-05 00:50:12 -0400

    Bump version to 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.crimeminister/autoremote "0.1.0"
+(defproject org.crimeminister/autoremote "0.1.1"
   :description "Send AutoRemote messages/notifications to Android devices"
   :url "http://github.com/crimeminister/clj-autoremote/"
   :license {:name "Eclipse Public License"
