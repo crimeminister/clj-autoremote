fb68a6b6a80765521b1961c4afc7d44fa05247c5
Robert Medeiros <robert@crimeminister.org>
2014-04-09 05:26:13 -0400

    Bump dep versions for clojure, validateur, http-kit, and tools.trace

--- project.clj
+++ project.clj
@@ -3,16 +3,16 @@
   :url "http://github.com/clojureist/clj-autoremote/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  ;; URI processing
                  [org.bovinegenius/exploding-fish "0.3.4"]
                  ;; Validations
-                 [com.novemberain/validateur "1.5.0"]
+                 [com.novemberain/validateur "2.0.0"]
                  ;; HTTP client/server
-                 [http-kit "2.1.13"]]
+                 [http-kit "2.1.18"]]
   :profiles {:dev {:dependencies [[fipp "0.4.1"]
                                   [org.clojure/tools.namespace "0.2.4"]
-                                  [org.clojure/tools.trace "0.7.6"]
+                                  [org.clojure/tools.trace "0.7.8"]
                                   [clj-stacktrace "0.2.7"]
                                   [clj-time "0.6.0"]]
                    :source-paths ["dev"]}}
