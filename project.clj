c9a7c398be5285414ab31921074aa7c19e40c7b7
Robert Medeiros <robert@crimeminister.org>
2014-04-09 05:26:04 -0400

    Start work on 0.1.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ca.clojureist/autoremote "0.1.2"
+(defproject ca.clojureist/autoremote "0.1.3-SNAPSHOT"
   :description "Send AutoRemote messages/notifications to Android devices"
   :url "http://github.com/clojureist/clj-autoremote/"
   :license {:name "Eclipse Public License"
