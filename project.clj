c1b80739d1524015b25fccc7f8e8110754b62601
Robert Medeiros <robert.medeiros@cbc.ca>
2014-07-10 10:47:39 -0400

    update project dependencies

--- project.clj
+++ project.clj
@@ -7,13 +7,13 @@
                  ;; URI processing
                  [org.bovinegenius/exploding-fish "0.3.4"]
                  ;; Validations
-                 [com.novemberain/validateur "2.0.0"]
+                 [com.novemberain/validateur "2.1.0"]
                  ;; HTTP client/server
                  [http-kit "2.1.18"]]
-  :profiles {:dev {:dependencies [[fipp "0.4.1"]
+  :profiles {:dev {:dependencies [[fipp "0.4.2"]
                                   [org.clojure/tools.namespace "0.2.4"]
                                   [org.clojure/tools.trace "0.7.8"]
-                                  [clj-stacktrace "0.2.7"]
+                                  [clj-stacktrace "0.2.8"]
                                   [clj-time "0.7.0"]]
                    :source-paths ["dev"]}}
   :min-lein-version "2.3.0")
