cedfd3235a6d5debf2e081921ba201f93e0f93ef
Robert Medeiros <robert@crimeminister.org>
2015-06-25 00:27:19 -0400

    bump dependency versions

--- project.clj
+++ project.clj
@@ -3,17 +3,17 @@
   :url "http://github.com/clojurist-ca/clj-autoremote/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-alpha3"]
+  :dependencies [[org.clojure/clojure "1.7.0-RC1"]
                  ;; URI processing
                  [org.bovinegenius/exploding-fish "0.3.4"]
                  ;; Validations
-                 [com.novemberain/validateur "2.3.1"]
+                 [com.novemberain/validateur "2.4.2"]
                  ;; HTTP client/server
                  [http-kit "2.1.19"]]
-  :profiles {:dev {:dependencies [[fipp "0.5.1"]
-                                  [org.clojure/tools.namespace "0.2.7"]
+  :profiles {:dev {:dependencies [[fipp "0.6.2"]
+                                  [org.clojure/tools.namespace "0.2.10"]
                                   [org.clojure/tools.trace "0.7.8"]
                                   [clj-stacktrace "0.2.8"]
-                                  [clj-time "0.8.0"]]
+                                  [clj-time "0.9.0"]]
                    :source-paths ["dev"]}}
   :min-lein-version "2.5.0")
